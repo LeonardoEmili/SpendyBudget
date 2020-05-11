bf451cc934f388912379973f7ce1c7e27ddf62e7
Leonardo Emili <leonardo.emili3@gmail.com>
2020-05-11 19:12:32 +0200

    Add internalization (dirty)

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,16 +1,60 @@
 <template>
   <div>
-    <b-navbar>
+    <b-navbar type="light">
       <b-navbar-brand to="/">
         <img src="../assets/logo.png" height="60px" width="60px" alt="." />
         SpendyBudget
       </b-navbar-brand>
 
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item to="/about">About us</b-nav-item>
-        <b-nav-item to="/login">Login</b-nav-item>
-        <b-nav-item to="/signup">Sign up</b-nav-item>
-      </b-navbar-nav>
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto" style="margin: 0; padding: 0">
+          <b-nav-item-dropdown text="English " right>
+            <b-dropdown-item variant="dark">
+              <img
+                src="../assets/flags/usa.png"
+                height="20px"
+                width="20px"
+                alt="."
+                style="margin-left: -15px; margin-right: 5px;"
+              /> English
+            </b-dropdown-item>
+            <b-dropdown-item variant="dark">
+              <img
+                src="../assets/flags/it.png"
+                height="20px"
+                width="20px"
+                alt="."
+                style="margin-left: -15px; margin-right: 5px;"
+              /> Italian
+            </b-dropdown-item>
+            <b-dropdown-item variant="dark">
+              <img
+                src="../assets/flags/de.png"
+                height="20px"
+                width="20px"
+                alt="."
+                style="margin-left: -15px; margin-right: 5px;"
+              /> German
+            </b-dropdown-item>
+            <b-dropdown-item variant="dark">
+              <img
+                src="../assets/flags/es.png"
+                height="20px"
+                width="20px"
+                alt="."
+                style="margin-left: -15px; margin-right: 5px;"
+              /> Spanish
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item to="/about">About Us</b-nav-item>
+          <b-nav-item to="/login">Login</b-nav-item>
+          <b-button variant="outline-success" style="padding: 0px 10px; margin-left: 10px;" pill>
+            <b-nav-item to="/signup">Sign up</b-nav-item>
+          </b-button>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
 
     <center>
@@ -29,8 +73,7 @@
 
 <script>
 export default {
-  name: "Index",
-  props: {}
+  name: "Index"
 };
 </script>
 
