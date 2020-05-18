55e83e5d0b9d4939ddf538df82cd693ff3481f25
Leonardo Emili <leonardo.emili3@gmail.com>
2020-05-19 01:28:46 +0200

    Add cards to Index and improve the layout

--- src/compiled-icons/index.js
+++ src/compiled-icons/index.js
@@ -1,9 +1,12 @@
 /* eslint-disable */
+require('./benefit')
 require('./bgift')
+require('./chat')
 require('./circle')
 require('./coinup')
 require('./gaming')
 require('./necktie')
 require('./salary2')
+require('./shield')
 require('./smartwatch')
 require('./tax')
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -49,6 +49,12 @@
       <h1>{{ $t('same_spending_tracker') }}</h1>
       <p id="description">{{$t('a_simple_expense_tracker')}}</p>
 
+      <b-button
+        pill
+        variant="success"
+        style="font-size: 20px; padding-left: 32px; padding-right: 32px; margin-top: 40px; margin-bottom: 40px"
+      >Get Started</b-button>
+
       <div id="macbook-wrapper">
         <img src="../assets/img_macbook.png" height="530px" width="800px" />
 
@@ -71,20 +77,92 @@
         </div>
       </div>
     </div>
+    <div style="margin-top: 100px; max-width: 1200px; margin-left: auto; margin-right: auto;">
+      <div
+        style="position: relative; width: 380px; display: inline-block; margin-left: 10px; margin-right: 10px;"
+      >
+        <b-card
+          class="shadow-sm mb-7 prova"
+          style="margin-right: 40px; border-radius: 8px; height: 320px;"
+        ></b-card>
+
+        <div
+          style="position: absolute; top: 0; left: 40px; right: 50px; padding-right: 30px; padding-top: 30px;"
+        >
+          <svgicon icon="benefit" width="70" height="70" color="#d32f2f" />
+          <p style="color: #444; font-size: 20px; font-weight: bold; margin-top: 40px;">Easy monitor</p>
+          <p
+            style="line-height:1.8; font-size: 17px;"
+          >Control over all of your expenses to help you save money.</p>
+        </div>
+      </div>
+
+      <div
+        style="position: relative; width: 380px; display: inline-block; margin-left: 10px; margin-right: 10px;"
+      >
+        <b-card
+          class="shadow-sm mb-7 prova"
+          style="margin-right: 40px; border-radius: 8px; height: 320px;"
+        ></b-card>
+
+        <div
+          style="position: absolute; top: 0; left: 40px; right: 50px; padding-right: 30px; padding-top: 30px;"
+        >
+          <svgicon icon="chat" width="70" height="70" color="#388e3c" />
+          <p
+            style="color: #444; font-size: 20px; font-weight: bold; margin-top: 40px;"
+          >Beautiful charts</p>
+          <p
+            style="line-height:1.8; font-size: 17px;"
+          >Check your balance through by our interactive charts.</p>
+        </div>
+      </div>
+
+      <div
+        style="position: relative; width: 380px; display: inline-block; margin-left: 10px; margin-right: 10px;"
+      >
+        <b-card
+          class="shadow-sm mb-7 prova"
+          style="margin-right: 40px; border-radius: 8px; height: 320px;"
+        ></b-card>
+
+        <div
+          style="position: absolute; top: 0; left: 40px; right: 50px; padding-right: 30px; padding-top: 30px;"
+        >
+          <svgicon icon="shield" width="70" height="70" color="#5c6bc0" />
+          <p style="color: #444; font-size: 20px; font-weight: bold; margin-top: 40px;">Safe place</p>
+          <p
+            style="line-height:1.8; font-size: 17px;"
+          >Our systems provide secure ways to store and manipulate data.</p>
+        </div>
+      </div>
+
+      <p style="color: #666; margin-top: 80px; margin-bottom: 60px;">
+        You’ve just started a new business or you wish to keep track of all your expenses on projects in a single place?
+        Then you’ve come to the right place!
+        Spendy is a simple and convenient service that allows keeping track of the money you spend on your startups and websites. Only your expenses and nothing else! We’ve decided not to overburden this service with invoices, income, and document flow, but rather concentrate only on expenses. Why would we do such a thing? Many startups are built by people from the ground up using their own money. Thus, it is important to remember how much money you’ve take out from the ‘family’ budget. When starting out it is vital to keep track of how much you spent and on what.
+        Spendy is suitable for any type of project, since you yourself choose the categories of your expenses, and their number is unlimited. Give this system a try and decide whether you’d like stay with us or choose another system.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 import * as utils from "../utils";
+import "../compiled-icons/";
 
 export default {
   name: "Index",
   created() {
+    window.addEventListener("scroll", this.handleScroll);
     // Initialize the locale to be used
     // TODO: check if this conditions still holds (probably not)
     this.userLocaleIndex = utils.locales.findIndex(
       locale => locale.iso === utils.getCurrentLocale()
     );
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
   data() {
     return {
@@ -108,6 +186,12 @@ export default {
     }
   },
   methods: {
+    handleScroll() {
+      // Any code to be executed when the window is scrolled
+      if (window.scrollY > 400) {
+        //let cards = document.getElementsByClassName("prova");
+      }
+    },
     updateLocale: function(index) {
       this.userLocaleIndex = index;
       utils.changeLanguage(this.currentISO);
@@ -120,7 +204,7 @@ export default {
 <style scoped>
 #wrapper {
   max-width: 800px;
-  margin-top: 50px;
+  margin-top: 60px;
   margin-left: auto;
   margin-right: auto;
   text-align: center;
@@ -179,7 +263,6 @@ export default {
 
 #description {
   margin-top: 10px;
-  margin-bottom: 30px;
   font-size: 18px;
 }
 
@@ -195,5 +278,16 @@ export default {
 #register-btn {
   padding: 0px 10px;
   margin-left: 20px;
+}
+
+.prova {
+  mask-image: none;
+  -webkit-mask-image: -webkit-gradient(
+    linear,
+    left bottom,
+    left top,
+    from(rgba(0, 0, 0, 1)),
+    to(rgba(0, 0, 0, 0.1))
+  );
 }
 </style>
