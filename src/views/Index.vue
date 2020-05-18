38717649e3aee9bc465a52e9b9985e5d2a85ae70
Leonardo Emili <leonardo.emili3@gmail.com>
2020-05-18 23:16:29 +0200

    Code refactor

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -21,17 +21,17 @@
                 height="20px"
                 width="20px"
                 alt="."
-                style="margin-left: -15px; margin-right: 8px;"
+                class="sel-flags"
               />
               <span :class="{ 'selected' : locale.iso == currentISO}">{{locale.name}}</span>
             </b-dropdown-item>
           </b-nav-item-dropdown>
 
-          <b-nav-item to="/about" class="border-right" style="margin-left: 10px;">{{$t('about_us')}}</b-nav-item>
+          <b-nav-item to="/about" class="border-right" id="about">{{$t('about_us')}}</b-nav-item>
           <b-nav-item to="/login">{{$t('login')}}</b-nav-item>
           <b-button
             variant="outline-success"
-            style="padding: 0px 10px; margin-left: 20px;"
+            id="register-btn"
             pill
             v-on:mouseover="hovering = true"
             v-on:mouseleave="hovering = false"
@@ -44,32 +44,33 @@
       </b-collapse>
     </b-navbar>
 
-    <center>
-      <div id="welcome-div">
-        {{$t()}}
-        <h1>{{ $t('same_spending_tracker') }}</h1>
-        <p>{{$t('a_simple_expense_tracker')}}</p>
+    <div id="wrapper">
+      {{$t()}}
+      <h1>{{ $t('same_spending_tracker') }}</h1>
+      <p id="description">{{$t('a_simple_expense_tracker')}}</p>
 
-        <div id="macbook-wrapper">
-          <img src="../assets/img_macbook.png" height="530px" width="800px" />
+      <div id="macbook-wrapper">
+        <img src="../assets/img_macbook.png" height="530px" width="800px" />
 
-          <div id="macbook-slider">
-            <b-carousel :interval="3000">
-              <b-carousel-slide>
-                <template v-slot:img>
-                  <img src="../assets/prova5.png" width="631px" height="400px" />
-                </template>
-              </b-carousel-slide>
-              <b-carousel-slide>
-                <template v-slot:img>
-                  <img src="../assets/prova8.png" width="631px" height="400px" />
-                </template>
-              </b-carousel-slide>
-            </b-carousel>
+        <div id="macbook-slider">
+          <b-carousel v-model="slide" :interval="3000">
+            <b-carousel-slide v-for="preview in previews" v-bind:key="preview">
+              <template v-slot:img>
+                <img :src="require('../assets/previews/' + preview)" width="631px" height="400px" />
+              </template>
+            </b-carousel-slide>
+          </b-carousel>
+          <div id="dot-frame">
+            <div
+              v-for="(item,index) in previews"
+              v-bind:key="index"
+              :class="index === slide ? 'selected-dot':'generic-dot'"
+              v-on:click="slide=index"
+            ></div>
           </div>
         </div>
       </div>
-    </center>
+    </div>
   </div>
 </template>
 
@@ -87,7 +88,9 @@ export default {
   },
   data() {
     return {
+      previews: ["dashboard.png", "settings.png"],
       userLocaleIndex: 0,
+      slide: 0,
       hovering: false
     };
   },
@@ -115,8 +118,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#welcome-div {
-  padding-top: 100px;
+#wrapper {
+  max-width: 800px;
+  margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  padding-bottom: 50px;
 }
 .selected {
   font-weight: bold;
@@ -137,5 +145,55 @@ export default {
   position: relative;
   width: 800px;
   height: 530px;
+}
+
+.generic-dot {
+  height: 12px;
+  width: 12px;
+  border-radius: 50%;
+  border: 3px solid #3f51b5;
+  display: inline-block;
+  margin: auto 8px;
+}
+
+.selected-dot {
+  height: 12px;
+  width: 12px;
+  border-radius: 50%;
+  background-color: #3f51b5;
+  display: inline-block;
+  margin: auto 8px;
+}
+
+.generic-dot:hover {
+  cursor: pointer;
+}
+
+.selected-dot:hover {
+  cursor: pointer;
+}
+
+#dot-frame {
+  margin-top: 80px;
+}
+
+#description {
+  margin-top: 10px;
+  margin-bottom: 30px;
+  font-size: 18px;
+}
+
+.sel-flags {
+  margin-left: -15px;
+  margin-right: 8px;
+}
+
+#about {
+  margin-left: 10px;
+}
+
+#register-btn {
+  padding: 0px 10px;
+  margin-left: 20px;
 }
 </style>
