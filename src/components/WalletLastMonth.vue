983f67606048a98a09fe69a9508ac349edf9ec2d
Leonardo Emili <leonardo.emili3@gmail.com>
2020-05-20 23:02:30 +0200

    Add balance to last week and last month

--- src/components/WalletLastMonth.vue
+++ src/components/WalletLastMonth.vue
@@ -1,59 +1,73 @@
 <template>
-      <b-card class="shadow-sm p-3 mb-5 wallet_card">
-     <h3>{{$t("last_month")}}</h3>
-        <br>
-        <line-chart :class="{chart: !isMobileView(), chart_mobile: isMobileView()}" 
-             :chartdata="lastMonthChartData" :options="null"></line-chart>
-        <br>
-      </b-card>
+  <b-card class="shadow-sm p-3 mb-5 wallet_card">
+    <h3>{{$t("last_month")}}</h3>
+    <p>{{$t('balance')}} {{convertFromEUR(getBalanceOfLastDays(30), walletCurrency).toFixed(2)}} {{walletCurrency}}</p>
+    <line-chart
+      :class="{chart: !isMobileView(), chart_mobile: isMobileView()}"
+      :chartdata="lastMonthChartData"
+      :options="null"
+    ></line-chart>
+  </b-card>
 </template>
 
 <script>
-import * as utils from "../utils"
-import LineChart from './LineChart.vue'
+import * as utils from "../utils";
+import LineChart from "./LineChart.vue";
 
 export default {
-    name: "WalletExpense",
-    components: {
-        lineChart: LineChart
+  name: "WalletExpense",
+  components: {
+    lineChart: LineChart
+  },
+  props: ["wallet"],
+  computed: {
+    walletTransactions: function() {
+      return this.wallet !== null && this.wallet.transactions !== undefined
+        ? this.wallet.transactions
+        : [];
     },
-    props: [
-        "wallet"
-    ],
-    computed: {
-               walletTransactions:  function () {return this.wallet !== null && this.wallet.transactions !== undefined
-           ? this.wallet.transactions : []},
-
-       
-        lastMonthChartData: function() {
-              let data = this.getBalanceMapOfLastDays(30)
-            return {
-                labels: data.map((point) => point.x),
+    walletCurrency: function() {
+      return this.wallet !== null ? this.wallet.currency : "";
+    },
+    lastMonthChartData: function() {
+      let data = this.getBalanceMapOfLastDays(30);
+      return {
+        labels: data.map(point => point.x),
         datasets: [
-            {
-              label: [this.$t("last_month")],
-              backgroundColor: '#8888ff',
-              data: data
-            }
-          ]
-            }
-        }
-       
-    },
-    methods: {
+          {
+            label: [this.$t("last_month")],
+            backgroundColor: "#8888ff",
+            data: data
+          }
+        ]
+      };
+    }
+  },
+  methods: {
     convertFromEUR(quantityEUR, currency) {
       return utils.convertFromEUR(quantityEUR, currency);
-        },
+    },
     isMobileView() {
       return utils.isMobileView();
     },
     getBalanceMapOfLastDays(daysNum) {
-        return utils.getBalanceMapOfLastDays(daysNum, this.walletTransactions)
+      return utils.getBalanceMapOfLastDays(daysNum, this.walletTransactions);
     },
+    getBalanceOfLastDays(daysNum) {
+      let ret = 0.0;
+      for (let transaction of this.walletTransactions) {
+        if (
+          transaction.instant._seconds / 60 / 60 / 24 >=
+          Date.now() / 1000 / 60 / 60 / 24 - daysNum
+        ) {
+          ret += transaction.amountEUR;
+        }
+      }
+      return ret;
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
--- src/components/WalletLastWeek.vue
+++ src/components/WalletLastWeek.vue
@@ -1,59 +1,73 @@
 <template>
-      <b-card class="shadow-sm p-3 mb-5 wallet_card">
-     <h3>{{$t("last_week")}}</h3>
-        <br>
-        <line-chart :class="{chart: !isMobileView(), chart_mobile: isMobileView()}" 
-             :chartdata="lastWeekChartData" :options="null"></line-chart>
-        <br>
-      </b-card>
+  <b-card class="shadow-sm p-3 mb-5 wallet_card">
+    <h3>{{$t("last_week")}}</h3>
+    <p>{{$t('balance')}} {{convertFromEUR(getBalanceOfLastDays(7), walletCurrency).toFixed(2)}} {{walletCurrency}}</p>
+    <line-chart
+      :class="{chart: !isMobileView(), chart_mobile: isMobileView()}"
+      :chartdata="lastWeekChartData"
+      :options="null"
+    ></line-chart>
+  </b-card>
 </template>
 
 <script>
-import * as utils from "../utils"
-import LineChart from './LineChart.vue'
+import * as utils from "../utils";
+import LineChart from "./LineChart.vue";
 
 export default {
-    name: "WalletExpense",
-    components: {
-        lineChart: LineChart
+  name: "WalletExpense",
+  components: {
+    lineChart: LineChart
+  },
+  props: ["wallet"],
+  computed: {
+    walletTransactions: function() {
+      return this.wallet !== null && this.wallet.transactions !== undefined
+        ? this.wallet.transactions
+        : [];
     },
-    props: [
-        "wallet"
-    ],
-    computed: {
-               walletTransactions:  function () {return this.wallet !== null && this.wallet.transactions !== undefined
-           ? this.wallet.transactions : []},
-
-       
-        lastWeekChartData: function() {
-              let data = this.getBalanceMapOfLastDays(7)
-            return {
-                labels: data.map((point) => point.x),
+    walletCurrency: function() {
+      return this.wallet !== null ? this.wallet.currency : "";
+    },
+    lastWeekChartData: function() {
+      let data = this.getBalanceMapOfLastDays(7);
+      return {
+        labels: data.map(point => point.x),
         datasets: [
-            {
-              label: [this.$t("last_week")],
-              backgroundColor: '#8888ff',
-              data: data
-            }
-          ]
-            }
-        }
-       
-    },
-    methods: {
+          {
+            label: [this.$t("last_week")],
+            backgroundColor: "#8888ff",
+            data: data
+          }
+        ]
+      };
+    }
+  },
+  methods: {
     convertFromEUR(quantityEUR, currency) {
       return utils.convertFromEUR(quantityEUR, currency);
-        },
-        isMobileView() {
+    },
+    isMobileView() {
       return utils.isMobileView();
     },
-     getBalanceMapOfLastDays(daysNum) {
-        return utils.getBalanceMapOfLastDays(daysNum, this.walletTransactions)
+    getBalanceMapOfLastDays(daysNum) {
+      return utils.getBalanceMapOfLastDays(daysNum, this.walletTransactions);
     },
+    getBalanceOfLastDays(daysNum) {
+      let ret = 0.0;
+      for (let transaction of this.walletTransactions) {
+        if (
+          transaction.instant._seconds / 60 / 60 / 24 >=
+          Date.now() / 1000 / 60 / 60 / 24 - daysNum
+        ) {
+          ret += transaction.amountEUR;
+        }
+      }
+      return ret;
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
